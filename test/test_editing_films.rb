8fe5f02dcb63a3b6acde80f6ce600f1c04cd3431
millerkatiej <millerkatiej@gmail.com>
2014-02-05 10:50:30 -0600

    last update before active record

--- test/test_editing_films.rb
+++ test/test_editing_films.rb
@@ -11,6 +11,16 @@ class TestEditingFilms < FilmTest
     assert_command_output expected, command
   end
 
+  def test_updating_a_record_that_exists
+    film = Film.new(name: "Persona", director: "Ingmar Bergman", year: 1966, country: "Sweden", language: "Swedish", distributor: "MGM Home Entertainment", date: "12/17/13", format: "DVD", rating: 9, notes: "Loved it")
+    film.save
+    id = film.id
+    command = "./filmtracker edit --id #{id} --name Persona --year 1966 --director 'Ingmar Bergmann' --country 'Sweden' --language 'Swedish' --distributor 'MGM Home Entertainment' --date '12/17/13' --format DVD --rating 9 --notes 'Loved it' --environment test"
+    expected = "Film #{id} is now named Persona, released in 1967, directed by Ingmar Bergmann, from Sweden, in Swedish, distributed by MGM Home Entertainment, viewed on 12/17/13, with a viewing format of DVD, given a personal rating of 9, and the following notes: Loved it."
+    # What about the db?
+    assert_command_output expected, command
+  end
+
   def test_attempting_to_update_a_nonexistant_record
     command = "./filmtracker edit --id 218903123980123 --name Persona --year 1966 --director 'Ingmar Bergman' --country Sweden --environment test"
     expected = "Film 218903123980123 couldn't be found."
